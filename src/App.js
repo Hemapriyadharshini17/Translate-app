4cbd0c9b09f86437aa3a709f6a6af7c4bf236404
Hemapriya17 <hema.learny@gmail.com>
2023-12-13 10:51:22 +0530

    App updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Field from "./Components/Field";
+import Language from "./Components/Language";
+import Translate from "./Components/Translate";
+import './styles.css'
 
 function App() {
+  const [language, setLanguage]= useState('es')
+  const [text, setText] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Field onChange={setText}/>
+    <Language language={language} onLanguageChange={setLanguage}/>
+    <hr/>
+    <Translate text={text} language={language}/>
     </div>
   );
 }
